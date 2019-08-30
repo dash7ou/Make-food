84d98024f0590a534e38909590010e7091bd673e
mohammed zourob <fxfddfno@gmail.com>
2019-08-30 16:24:49 +0300

    add finial style

--- src/Recipes/Recipes.js
+++ src/Recipes/Recipes.js
@@ -3,14 +3,16 @@ import "./recipes.css";
 
 const Recipe = props => {
   return (
-    <div className={recipe}>
+    <div className="recipe">
       <h1>{props.title}</h1>
       <ol>
         {props.ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{props.calories}</p>
+      <p>
+        <span>Calories:</span> {props.calories}
+      </p>
       <img src={`${props.image}`} alt={`${props.label}`} />
     </div>
   );
