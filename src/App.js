1b90cbcc5eeb4ef63116bdd0a4595e71b5bedbfe
mohammed zourob <fxfddfno@gmail.com>
2019-08-30 14:43:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APPID = "715ccf54";
+  const APPKEY = "43ebd5b10e8c6ffe2469e8f916d26117";
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APPID}&app_key=${APPKEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
